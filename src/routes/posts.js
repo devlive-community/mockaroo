bd326180e7d19167cbf180ca4c870d9e42c2dafa
qianmoQ <shicheng@devlive.org>
2025-02-04 08:51:23 +0800

    支持 patch

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {generatePosts, generatePost} = require('../utils/posts');
 
-// 获取文章列表
 router.get('/', (req, res) => {
     // 从查询参数获取 limit，如果没有则默认为 100
     const limit = parseInt(req.query.limit) || 100;
@@ -10,24 +9,24 @@ router.get('/', (req, res) => {
     res.json(generatePosts(limit));
 });
 
-// 获取单个文章
 router.get('/:id', (req, res) => {
     res.json(generatePost(req.params.id));
 });
 
-// 创建文章
 router.post('/', (req, res) => {
     res.json(generatePost(req.body));
 });
 
-// 更新文章
 router.put('/:id', (req, res) => {
     res.json(generatePost(req.body));
 });
 
-// 删除文章
 router.delete('/:id', (req, res) => {
     res.json(generatePost(req.params.id));
+});
+
+router.patch('/:id', (req, res) => {
+    res.json(generatePost(req.body));
 });
 
 module.exports = router;
