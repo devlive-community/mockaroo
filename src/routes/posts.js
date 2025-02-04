1aa115ed7587126d1a3ca09d322092035b0c6d48
qianmoQ <shicheng@devlive.org>
2025-02-04 08:17:22 +0800

    支持服务自动部署

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -7,15 +7,7 @@ router.get('/', (req, res) => {
     // 从查询参数获取 limit，如果没有则默认为 100
     const limit = parseInt(req.query.limit) || 100;
 
-    // 生成文章数据
-    const posts = generatePosts(limit);
-
-    // 返回响应
-    res.json({
-        code: 200,
-        message: 'success',
-        data: posts
-    });
+    res.json(generatePosts(limit));
 });
 
 module.exports = router;
